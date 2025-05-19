a1fd263eeb48daebe5affb280eac05de759abfe3
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-05-19 14:59:01 -0400

    Add rest of methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -13,6 +13,75 @@ module Enumerable
     end
     return array
   end
+  def my_all?
+    array = []
+    for item in self
+      if yield(item)
+        array.push(true) 
+      else
+        array.push(false)
+      end
+    end
+    if Array.new(self.length, true) == array
+      return true
+    else
+      return false
+    end
+  end
+  def my_any?
+    array = []
+    for item in self
+      if yield(item)
+        array.push(true) 
+      else
+        array.push(false)
+      end
+    end
+    if array.include?(true)
+      return true
+    else
+      return false
+    end
+  end
+  def my_none?
+    array = []
+    for item in self
+      if yield(item)
+        array.push(true) 
+      else
+        array.push(false)
+      end
+    end
+    if Array.new(self.length, false) == array
+      return true
+    else
+      return false
+    end
+  end
+  def my_count
+    counter = 0
+    for item in self
+      if block_given?
+        counter +=1 if yield(item)
+      else
+        counter += 1
+      end
+    end
+    return counter
+  end
+  def my_map
+    array = []
+    for item in self
+      array.push(yield(item))
+    end
+    return array
+  end
+  def my_inject(total)
+    for value in self
+     total = yield(total, value)
+    end
+    total
+  end
 end
 
 # You will first have to define my_each
